5eed202d6ad1ab1751163af855469c3b9a3c8b34
zhangyuhao <zhangyuhao1158@sina.com>
2015-12-20 14:28:22 +0800

    Initial Commit

---
+++ GaoDeMapDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GaoDeMapDemo
+//
+//  Created by zyh on 15/12/20.
+//  Copyright © 2015年 zyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GaoDeMapDemoTests/GaoDeMapDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  GaoDeMapDemoTests.m
+//  GaoDeMapDemoTests
+//
+//  Created by zyh on 15/12/20.
+//  Copyright © 2015年 zyh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GaoDeMapDemoTests : XCTestCase
+
+@end
+
+@implementation GaoDeMapDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
